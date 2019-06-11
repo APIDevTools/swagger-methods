49b8fd7933fde92919339741885f28d83b8b2280
James Messinger <james.messinger@gmail.com>
2019-06-11 18:46:54 -0500

    Updated to ES6 syntax

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,36 +1,36 @@
 "use strict";
 
-var swaggerMethods = require("../"),
-    swaggerSchema = require("swagger-schema-official/schema"),
-    httpMethods = require("methods"),
-    expect = require("chai").expect;
+const swaggerMethods = require("../");
+const swaggerSchema = require("swagger-schema-official/schema");
+const httpMethods = require("methods");
+const { expect } = require("chai");
 
-describe("swagger-methods", function () {
-  it("should be a non-empty array", function () {
+describe("swagger-methods", () => {
+  it("should be a non-empty array", () => {
     expect(swaggerMethods).to.be.an("array").with.length.above(0);
   });
 
-  it("should be all lowercase strings", function () {
+  it("should be all lowercase strings", () => {
     swaggerMethods.forEach(function (method) {
       expect(method).to.be.a("string").and.equal(method.toLowerCase());
     });
   });
 
-  it("should only have valid HTTP methods", function () {
+  it("should only have valid HTTP methods", () => {
     swaggerMethods.forEach(function (method) {
       expect(httpMethods).to.include(method);
     });
   });
 
-  it("should match the Swagger 2.0 schema", function () {
+  it("should match the Swagger 2.0 schema", () => {
     // "pathItem" is the Swagger object that defines the allowed HTTP methods
     // (see https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md#path-item-object)
-    var pathItemProps = swaggerSchema.definitions.pathItem.properties;
+    let pathItemProps = swaggerSchema.definitions.pathItem.properties;
 
     // We only care about the "operation" properties
     // (see https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md#operationObject)
-    var operations = Object.keys(pathItemProps).filter(function (key) {
-      var pathItemProp = pathItemProps[key];
+    let operations = Object.keys(pathItemProps).filter(function (key) {
+      let pathItemProp = pathItemProps[key];
       return pathItemProp.$ref === "#/definitions/operation";
     });
 
